6d7e8669cc1f0a4c4be0e12c0c90444de4c07ed0
Nick <nodhiambo01@gmail.com>
2024-10-06 09:17:45 +0300

    Add color notifications

--- task_manager.py
+++ task_manager.py
@@ -2,6 +2,9 @@ from datetime import datetime, timedelta
 import csv
 import os
 import argparse
+from colorama import init, Fore, Style
+
+init(autoreset=True)
 
 class Task:
     """A model to store task details"""
@@ -10,14 +13,14 @@ class Task:
         try:
             self.due_date = datetime.strptime(due_date, '%Y-%m-%d')
         except ValueError:
-            raise ValueError(f'Incorrect data format {due_date}. It should be YYYY-MM-DD')
+            raise ValueError(f'Incorrect date format {due_date}. It should be YYYY-MM-DD')
         self.priority = priority
         self.tags = tags
         self.completed = completed
 
 
     def __str__(self):
-        status = "Done" if self.completed else "Pending"
+        status = Fore.GREEN + "Done" if self.completed else Fore.YELLOW + "Pending"
         return f'{self.description} | Due: {self.due_date.date()} | Priority: {self.priority} | Tags: {self.tags} | Status: {status}'
     
 def add_task(description, due_date, priority, tags=""):
@@ -25,7 +28,7 @@ def add_task(description, due_date, priority, tags=""):
     try:
         task = Task(description, due_date, priority, tags)
     except ValueError as e:
-        print(e)
+        print( Fore.RED + str(e))
         return
     
     file_exists = os.path.exists('tasks.csv')
@@ -34,16 +37,15 @@ def add_task(description, due_date, priority, tags=""):
         if not file_exists:
             writer.writerow(['Description', 'Due Date', 'Priority', 'Tags', 'Completed'])
         writer.writerow([description, due_date, priority, tags, task.completed])
-    print(f"Task: {description} added successfully under {tags}")
+    print(Fore.GREEN + f"Task: {description} added successfully under {tags}")
 
 
 def view_tasks(reminder=False):
     """This functions reads tasks from csv and displays them"""
     tasks = []
-    reminders = []
 
     if not os.path.exists('tasks.csv'):
-        print('No tasks found. Please add a task first')
+        print(Fore.RED + 'No tasks found. Please add a task first')
         return
 
 
@@ -54,28 +56,25 @@ def view_tasks(reminder=False):
             try:
                 description, due_date, priority, tags, completed = row
             except ValueError:
-                print('No tasks added yet')
+                print(Fore.RED + 'No tasks added yet')
                 return
             task = Task(description, due_date, priority, tags, completed == 'True')
             tasks.append(task)
     
     if not tasks:
-        print("No tasks available")
+        print(Fore.RED + "No tasks available")
         return
     
     # sort by due date
-    tasks.sort(key=lambda t: t.due_date)
+    # tasks.sort(key=lambda t: t.due_date)
 
     today = datetime.now()
 
     if reminder:
-        print("Upcomimg tasks...")
+        print(Fore.CYAN + "Tasks due in 2 days...")
         for task in tasks:
             if task.due_date <= today + timedelta(days=2) and not task.completed:
-                print(f'Reminder: {task}')
-            else:
-                print("No tasks due within the next two days")
-                break
+                print(Fore.YELLOW + f'Reminder: {task}')
 
     else:
         for idx, task in enumerate(tasks, start=1):
@@ -87,7 +86,7 @@ def mark_tasks_complete(task_index):
     tasks = []
 
     if not os.path.exists('tasks.csv'):
-        print('No tasks available. Add a task first')
+        print(Fore.RED + 'No tasks available. Add a task first')
         return
 
     with open('tasks.csv', 'r', newline='') as f:
@@ -100,15 +99,15 @@ def mark_tasks_complete(task_index):
         with open('tasks.csv', 'w', newline='') as f:
             writer = csv.writer(f)
             writer.writerows(tasks)
-        print(f'Task {task_index + 1} marked as complete')
+        print(Fore.GREEN + f'Task {task_index} marked as complete')
     else:
-        print('No such task. View tasks available by number')
+        print(Fore.RED + 'No such task. View tasks available by number')
 
 
 def delete_task(task_index):
     """Deletes a task by index"""
     if not os.path.exists('tasks.csv'):
-        print("Tasks not found")
+        print(Fore.RED + "Tasks not found")
         return
     
     tasks = []
@@ -123,9 +122,9 @@ def delete_task(task_index):
         with open('tasks.csv', 'w', newline='') as f:
             writer = csv.writer(f)
             writer.writerows(tasks)
-        print(f'Deleted {deleted_task[0]} due on {deleted_task[1]}')
+        print(Fore.MAGENTA + f'Deleted {deleted_task[0]} due on {deleted_task[1]}')
     else:
-        print(f"Invalid task number. Please select a task number between 1 and {len(tasks)}")
+        print(Fore.RED + f"Invalid task number. Please select a task number between 1 and {len(tasks) - 1}")
 
 
 
@@ -145,9 +144,8 @@ def main():
     args = parse_arguments()
 
     if args.add:
-        print(args.add)
         if len(args.add) < 3:
-            print("Please provide a task description, due date and priority")
+            print(Fore.RED + "Please provide a task description, due date and priority")
             return
         description = args.add[0]
         due_date = args.add[1]
@@ -158,9 +156,9 @@ def main():
     elif args.view:
         view_tasks()
     elif args.complete is not None:
-        mark_tasks_complete(args.complete - 1)
+        mark_tasks_complete(args.complete)
     elif args.delete is not None:
-        delete_task(args.delete - 1)
+        delete_task(args.delete)
     elif args.reminders:
         view_tasks(reminder=True)
 
